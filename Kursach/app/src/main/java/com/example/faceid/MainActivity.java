82dc2d5401c6f1fbb79db6116cc27ab98033cdd1
BlockiyDmitriy <51110104+BlockijDmitrijSergeevich@users.noreply.github.com>
2020-04-20 00:31:27 +0300

    После переустановки AS

--- Kursach/app/src/main/java/com/example/faceid/MainActivity.java
+++ Kursach/app/src/main/java/com/example/faceid/MainActivity.java
@@ -1,65 +1,140 @@
 package com.example.faceid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Bundle;
-import android.util.SparseArray;
-import android.widget.ImageView;
-import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    File directory;
+    final int TYPE_PHOTO = 1;
+
+    final int REQUEST_CODE_PHOTO = 1;
+
+    final String TAG = "myLogs";
+
+    ImageView ivPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView myImageView = (ImageView) findViewById(R.id.image);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable = true;
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.drawable.test1,
-                options);
+        createDirectory();
+        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
 
-        Paint myRectPaint = new Paint();
-        myRectPaint.setStrokeWidth(5);
-        myRectPaint.setColor(Color.RED);
-        myRectPaint.setStyle(Paint.Style.STROKE);
+        Button faceId_btn = (Button) findViewById(R.id.faceId_btn);
+        Button choiceImg_btn = (Button) findViewById(R.id.choiceImg_btn);
 
-        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
-        Canvas tempCanvas = new Canvas(tempBitmap);
-        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+        faceId_btn.setOnClickListener(this);
+        choiceImg_btn.setOnClickListener(this);
+    }
 
-        FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false).build();
-        if (!faceDetector.isOperational()) {
-            Toast.makeText(this, "Cold not set up the Face Detector!", Toast.LENGTH_LONG).show();
-            return;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.faceId_btn: {
+                ImageView myImageView = (ImageView) findViewById(R.id.image);
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inMutable = true;
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.photo,
+                        options);
+
+                Paint myRectPaint = new Paint();
+                myRectPaint.setStrokeWidth(15);
+                myRectPaint.setColor(Color.RED);
+                myRectPaint.setStyle(Paint.Style.STROKE);
+
+                Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+                Canvas tempCanvas = new Canvas(tempBitmap);
+                tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+                FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false).build();
+                if (!faceDetector.isOperational()) {
+                    Toast.makeText(this, "Cold not set up the Face Detector!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Face> faces = faceDetector.detect(frame);
+
+                new DrowRectangle(faces, tempCanvas, myRectPaint);
+
+                myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+            }
+            case R.id.choiceImg_btn: {
+
+            }
         }
+    }
 
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray <Face> faces = faceDetector.detect(frame);
-
-        for (int i = 0; i < faces.size(); i++) {
-            Face thisFace = faces.valueAt(i);
-            float x1 = thisFace.getPosition().x;
-            float y1 = thisFace.getPosition().y;
-            float x2 = x1 + thisFace.getWidth();
-            float y2 = y1 + thisFace.getHeight();
-            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+    public void onClickPhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, generateFileUri(TYPE_PHOTO));
+        startActivityForResult(intent, REQUEST_CODE_PHOTO);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_CODE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                if (intent == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Photo uri: " + intent.getData());
+                    Bundle bndl = intent.getExtras();
+                    if (bndl != null) {
+                        Object obj = intent.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            Log.d(TAG, "bitmap " + bitmap.getWidth() + " x " + bitmap.getHeight());
+                            ivPhoto.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.d(TAG, "Canceled");
+            }
         }
+    }
+    private Uri generateFileUri(int type) {
+        File file = null;
+        if (type == TYPE_PHOTO) {
+            file = new File(directory.getPath() + "/" + "photo_" + System.currentTimeMillis() + ".jpg");
+        }
+        Log.d(TAG, "fileName = " + file);
+        return Uri.fromFile(file);
+    }
 
-        myImageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+    private void createDirectory() {
+        directory = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyFolder");
+        if (!directory.exists())
+            directory.mkdirs();
     }
 }
